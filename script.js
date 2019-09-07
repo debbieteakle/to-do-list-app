d24a8f0fa2cffa4ec448f3637f9ef47a193e7497
Debbie Teakle <debbieteakle@gmail.com>
2019-09-07 18:04:56 +1000

    New delete buttons have class of delete

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ function inputLength(){
 	return userinput.value.length;
 }
 
+
+//clean this function up
 function createListElement(){
 		var li = document.createElement("li");
 		var deleteButton = document.createElement("button");
+		deleteButton.classList.add("delete");
 		deleteButton.innerText= "Delete";
 		var newListItem = document.createTextNode(userinput.value + " ");
 		li.appendChild(newListItem);
@@ -27,10 +30,18 @@ function addItemAfterClick() {
 function addItemAfterEnter(event) {
 	if (inputLength() > 0 && event.keyCode === 13) {
 		createListElement();
-	}// body...
+	}
+}
+
+function removeItemAfterDelete(){
+	//do opposite of createListElement
+	//remove list element and it's button
+
+
 }
 
 button.addEventListener("click", addItemAfterClick)
 
 userinput.addEventListener("keypress", addItemAfterEnter)
 
+deleteButton.addEventListener("click", removeItemAfterDelete)
