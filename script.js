151b1dfdf9e79572432058f9bbeac52c45ec63bc
Debbie Teakle <debbieteakle@gmail.com>
2019-09-07 17:23:34 +1000

    Delete buttons appearing as each new item is added

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ var button = document.querySelector("#add");
 var userinput = document.querySelector("#userinput");
 var ul = document.querySelector("ul");
 
+
 function inputLength(){
 	return userinput.value.length;
 }
 
 function createListElement(){
 		var li = document.createElement("li");
+		var deleteButton = document.createElement("button");
+		deleteButton.innerText= "Delete";
 		var newListItem = document.createTextNode(userinput.value);
 		li.appendChild(newListItem);
+		li.appendChild(deleteButton);
 		ul.appendChild(li);
 		userinput.value = "";
 }
