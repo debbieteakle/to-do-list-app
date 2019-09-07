927cc33481d3db1e3862e21f034c15db0c2a722a
Debbie Teakle <debbieteakle@gmail.com>
2019-09-07 16:10:51 +1000

    adding starting project: list items can be added by clicking button or clicking Enter

---
+++ script.js
@@ -0,0 +1,32 @@
+var button = document.querySelector("#add");
+var userinput = document.querySelector("#userinput");
+var ul = document.querySelector("ul");
+
+function inputLength(){
+	return userinput.value.length;
+}
+
+function createListElement(){
+		var li = document.createElement("li");
+		var newListItem = document.createTextNode(userinput.value);
+		li.appendChild(newListItem);
+		ul.appendChild(li);
+		userinput.value = "";
+}
+
+function addItemAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addItemAfterEnter(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}// body...
+}
+
+button.addEventListener("click", addItemAfterClick)
+
+userinput.addEventListener("keypress", addItemAfterEnter)
+
