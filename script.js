455b405f7f3b2d7123a5162c4df072e88f358317
Debbie Teakle <debbieteakle@gmail.com>
2019-09-08 12:30:31 +1000

    User can mark items as done or delete a list item

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 var button = document.querySelector("#add");
 var userinput = document.querySelector("#userinput");
 var ul = document.querySelector("ul");
+var deleteButton = document.querySelectorAll(".delete");
 
 
 function inputLength(){
 	return userinput.value.length;
 }
 
+function createDeleteButton(){
+	var deleteButton = document.createElement("button");
+	deleteButton.classList.add("delete");
+	deleteButton.innerText= "Delete";
+	return deleteButton;
+}
 
-//clean this function up
+
 function createListElement(){
 		var li = document.createElement("li");
-		var deleteButton = document.createElement("button");
-		deleteButton.classList.add("delete");
-		deleteButton.innerText= "Delete";
+		var deleteButton = createDeleteButton();
 		var newListItem = document.createTextNode(userinput.value + " ");
 		li.appendChild(newListItem);
 		li.appendChild(deleteButton);
 		ul.appendChild(li);
 		userinput.value = "";
+		deleteButton.addEventListener("click", function(){
+			this.parentElement.remove();
+			})
+		console.log(li);
+		li.addEventListener("click", function(){
+			this.classList.toggle("done");
+		})
 }
 
 function addItemAfterClick() {
@@ -33,15 +45,8 @@ function addItemAfterEnter(event) {
 	}
 }
 
-function removeItemAfterDelete(){
-	//do opposite of createListElement
-	//remove list element and it's button
-
-
-}
 
 button.addEventListener("click", addItemAfterClick)
 
 userinput.addEventListener("keypress", addItemAfterEnter)
 
-deleteButton.addEventListener("click", removeItemAfterDelete)
